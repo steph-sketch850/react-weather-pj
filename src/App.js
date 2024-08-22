bf56d920248524b4fc767dcb4f38daa876bc2424
steph-sketch850 <145380074+steph-sketch850@users.noreply.github.com>
2024-08-22 10:45:23 -0400

    Wrapped footer in div class

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import "./App.css";
 
 export default function App() {
   return (
-    <footer>
-      This project was coded by{" "}
-      <a href="https://github.com/steph-sketch850">Stephanie Rodriguez</a> and
-      is open-sourced on{" "}
-      <a href="https://github.com/steph-sketch850/react-weather-pj">GitHub</a>{" "}
-      and hosted on <a href="https://first-react-pj.netlify.app/">Netlify</a>.
-    </footer>
+    <div className="App">
+      <div className="container">
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/steph-sketch850">Stephanie Rodriguez</a>{" "}
+          and is open-sourced on{" "}
+          <a href="https://github.com/steph-sketch850/react-weather-pj">
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://first-react-pj.netlify.app/">Netlify</a>.
+        </footer>
+      </div>
+    </div>
   );
 }
